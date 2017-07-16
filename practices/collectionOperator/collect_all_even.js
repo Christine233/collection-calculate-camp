8c5ffa2249335faed3c5e5dc3410552e4a89f9f9
Christine233 <1040551136@qq.com>
2017-07-16 14:20:54 +0800

    finish

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,6 +2,13 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  let even = [];
+  for(let i of collection){
+    if(i % 2 == 0){
+      even.push(i);
+    }
+  }
+  //console.log(even);
+  return even;
 }
-
-module.exports = collect_all_even;
+//module.exports = collect_all_even;
